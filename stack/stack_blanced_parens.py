dfc375d52412a798e492116ccf408fa6b6b281cf
rezaizadijahromi <rezaizadij2000@gmail.com>
2020-08-05 12:44:49 +0430

    Stack() and blanced problem

---
+++ stack/stack_blanced_parens.py
@@ -0,0 +1,40 @@
+from stack import Stack
+
+def is_match(p1 ,p2):
+    if p1 == '(' and p2  == ')':
+        return True
+    
+    elif p1 == '[' and  p2 == ']':
+        return True
+    
+    elif p1 =='{' and p2 == '}':
+        return True
+    
+    else:
+        return False
+
+def is_paren_balanced(paren_string):
+    s = Stack()
+    is_blanced = True
+    index = 0 
+    while index < len(paren_string) and is_blanced:
+        paren = paren_string[index]
+        if paren in "({[":
+            s.push(paren)
+        else:
+            if s.is_empty():
+                is_blanced = False
+            else:
+                top = s.pop()
+                if not is_match(top, paren):
+                    is_blanced = False
+        index += 1
+
+    if s.is_empty() and is_blanced:
+        return True
+    else:
+        return False
+
+
+print(is_paren_balanced("{{()}}"))
+print(is_paren_balanced("{{()}"))
