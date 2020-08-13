ac9f80309349cb0eac4d222f9a7a7e2ac2675845
rezaizadijahromi <rezaizadij2000@gmail.com>
2020-08-13 13:13:19 +0430

    Recursive br search

--- binary search algorithm/br_search.py
+++ binary search algorithm/br_search.py
@@ -14,13 +14,29 @@ def binary_search_iterative(data, target):
     while low <= high:
         mid = (low + high) // 2
         if target == data[mid]:
-            print("founded")
+            print("Found")
             return True
         elif target < data[mid]:
             high = mid - 1
         else:
             low = mid + 1
-    print("Not find")
+    print("Not found")
     return False
 
 obj = binary_search_iterative(data=data, target=target)
+
+def binary_search_recursive(data, target, low, high):
+    if low > high:
+        print("NOt found")
+        return False
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            print("Found")
+            return True
+        elif target < data[mid]:
+            return binary_search_recursive(data, target, low, mid - 1)
+        else:
+            return binary_search_recursive(data, target, mid+1, high)
+
+obj1 = binary_search_recursive(data, target, 0, len(data)-1)
