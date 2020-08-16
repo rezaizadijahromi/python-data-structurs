21e34803a4279025b6bf39e19957dd4edb0bd7dc
rezaizadijahromi <rezaizadij2000@gmail.com>
2020-08-16 22:25:03 +0430

    Pre order in binary tree

---
+++ binary tree/BinaryTree.py
@@ -0,0 +1,47 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        else:
+            print(f"Traversal type " + str(traversal_type) + " is not supported.")
+            return False
+
+    def preorder_print(self, start, traversal):
+        if start:
+            traversal += (str(start.value) + "-")
+            # print(traversal)
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
+ 
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+tree.root.right.right.right = Node(8)
+
+# print(tree.print_tree("preorder"))
+print(tree.preorder_print(tree.root, ""))
+
+
+
+# def height(self, node):
+#     if node is None:
+#         return -1
+#     left_height = self.height(node.left)
+#     right_height = self.height(node.right)
+
+#     return 1 + max(left_height, right_height)
