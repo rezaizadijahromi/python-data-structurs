caa35d9103c0bf3e62549fb98929536f25c568f3
rezaizadijahromi <rezaizadij2000@gmail.com>
2020-08-18 23:10:28 +0430

    Fix indent

--- binary tree/BinaryTree.py
+++ binary tree/BinaryTree.py
@@ -87,13 +87,13 @@ class BinaryTree(object):
 
         return traversal
 
-        def height(self, node):
-            if node is None:
-                return -1
-            left_height = self.height(node.left)
-            right_height = self.height(node.right)
+    def height(self, node):
+        if node is None:
+            return -1
+        left_height = self.height(node.left)
+        right_height = self.height(node.right)
 
-            return 1 + max(left_height, right_height)
+        return 1 + max(left_height, right_height)
 
 
 
