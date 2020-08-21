adf2f9f4800e72688e0e08b1ff38ce2685a99b92
rezaizadijahromi <rezaizadij2000@gmail.com>
2020-08-21 20:45:40 +0430

    calculate size of tree itrative

--- binary tree/BinaryTree.py
+++ binary tree/BinaryTree.py
@@ -126,6 +126,22 @@ class BinaryTree(object):
 
         return 1 + max(left_height, right_height)
 
+    def size(self):
+        if self.root is None:
+            return 0
+
+        stack = Stack()
+        stack.push(self.root)
+        size = 1
+        while stack:
+            node = stack.pop()
+            if node.left:
+                size += 1
+                stack.push(node.left)
+            if node.right:
+                size += 1
+                stack.push(node.right)
+        
    
 
  
