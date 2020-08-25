236ecff3ad1733f8ef1c406b39397592fd2c19da
rezaizadijahromi <rezaizadij2000@gmail.com>
2020-08-25 14:37:59 +0430

    Finding fixed point with binary search algorithm

--- binary search algorithm/find_fixed_point.py
+++ binary search algorithm/find_fixed_point.py
@@ -5,3 +5,24 @@ def find_fixed_point_linear(A):
     return None
 
 
+def find_fixed_point(A):
+    low = 0 
+    high = len(A)
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if A[mid] < mid:
+            low = mid + 1
+        elif A[mid] > mid:
+            high = mid - 1
+
+        else:
+            return A[mid]
+
+    return None
+
+
+A = [-10, -5, 0, 3, 7]
+A1 = [0, 2, 5, 8, 17]
+print(find_fixed_point(A1))
